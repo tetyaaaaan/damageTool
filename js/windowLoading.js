5ae2df286a99a89b38788e998354e6990e59cc9a
tetyaaaaan <54570470+tetyaaaaan@users.noreply.github.com>
2023-04-17 10:01:40 +0900

    Update windowLoading.js

--- js/windowLoading.js
+++ js/windowLoading.js
@@ -1,1 +1,7 @@
-
+window.onload = function () {
+  //ここでローディング画像を表示していたDIVを非表示にする
+  document.getElementById('load').style.display = 'none';
+		
+	//ここで本体を表示にさせる
+	document.getElementById('wrapper').style.display = 'block';	
+}
