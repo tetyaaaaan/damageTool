8aa1b0714563e1f39a41918123f272418bd5bfb4
tetyaaaaan <54570470+tetyaaaaan@users.noreply.github.com>
2023-04-23 14:15:19 +0900

    Update user_count.js

--- js/user_count.js
+++ js/user_count.js
@@ -1,6 +1,28 @@
 //トラッキングID
 var gTrackingId = 'G-S82LRCKEPS';
 
+
+// ユーザ数を取得してHTML要素に表示する関数
+function updateUserCount(userCount) {
+  document.getElementById('userCount').innerText = userCount;
+}
+
+// Google Analyticsのユーザ数を取得する関数
+function fetchUserCount() {
+  gtag('get', gTrackingId, 'users', function(response) {
+    // ユーザ数の取得が完了した後にHTML要素に表示
+    var userCount = response['users'];
+    updateUserCount(userCount);
+  });
+}
+
+// ページ読み込み時にユーザ数を取得
+window.addEventListener('load', function() {
+  fetchUserCount();
+});
+
+
+/*
 // ユーザ数を取得してHTML要素に表示する関数
 function updateUserCount(userCount) {
   document.getElementById('userCount').innerText = userCount;
@@ -23,3 +45,4 @@ function fetchUserCount() {
 window.addEventListener('load', function() {
   fetchUserCount();
 });
+*/
